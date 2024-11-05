fbc2661efc7dd46810fc2b7a5d21a62c97a91599
Josh Moran <josh@lovingfamily.co.uk>
2024-11-05 10:10:32 +0000

    bug fixing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 // Set favicon logo
-<<<<<<< HEAD
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // Import CSS File
 import './App.css';
 // Import Modules for functionality
 import React, { useState, setValue } from 'react';
-=======
 // import logo from './logo.svg';
 // Import CSS File
 import './App.css';
 // Import Modules for functionality
 import React, { useState } from 'react';
->>>>>>> c70f7fe (Bug fixing)
 
 // Set previous calculation to null 
 let currentCalc = '';
@@ -30,37 +27,28 @@ function checkEmpty( stringEl ) {
   let toString = String( stringEl );
 
   // Check if the string is not empty, null or undefined
-<<<<<<< HEAD
   if (  toString != '' || toString != null || toString != 'undefined') {
-=======
-  if (  toString !== '' || toString != null || toString !== 'undefined') {
->>>>>>> c70f7fe (Bug fixing)
+    if (  toString !== '' || toString != null || toString !== 'undefined') {
     // If not, try to remove the last character
-    try { 
-      // Remove the last character from the string and return the value
-      return stringEl.slice(0, -1 ) ;
-    } catch ( e ) {
-      // On error, just return
-      return;
+      try { 
+        // Remove the last character from the string and return the value
+        return stringEl.slice(0, -1 ) ;
+      } catch ( e ) {
+        // On error, just return
+        return;
+      }
+    } else {
+      // If it is empty, null or undefined return the previous calculation
+      return currentCalc;
     }
-  } else {
-    // If it is empty, null or undefined return the previous calculation
-    return currentCalc;
-  }
 }
-
+}
 // Function to check if the input is empty or has something in it 
 function checkValue ( currentVal, targetEl ) {
   // Check if the target element is empty or has something in it
-<<<<<<< HEAD
-  if ( targetEl == '' || targetEl == null ) {
-    // If it is, check if the previous calculation is not 0 and greater than 0 
-    if ( currentCalc == 0 && currentCalc > 0 ){ 
-=======
   if ( targetEl === '' || targetEl === null ) {
     // If it is, check if the previous calculation is not 0 and greater than 0 
     if ( currentCalc === 0 && currentCalc > 0 ){ 
->>>>>>> c70f7fe (Bug fixing)
       // If it is, just return the current value being inputted
       return  currentVal;
     } else {
